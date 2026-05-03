fdfd1de363348f18a70bc19c5e0db70f6198be77
U-riani <sandropapiashvili@gmail.com>
2026-05-04 01:49:15 +0400

    s

--- index.js
+++ index.js
@@ -22,17 +22,20 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://ska-bar.netlify.app",
-      -"https://admin-ska-bar.netlify.app",
-    ],
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://ska-bar.netlify.app",
+    "https://admin-ska-bar.netlify.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
